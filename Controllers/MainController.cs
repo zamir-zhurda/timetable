c1dfe173b17660840153894adb71d657a6f63b6a
zamir zhurda <zamirzhurda@zamirs-MacBook-Pro.local>
2020-01-21 18:22:57 +0100

    Added some endpoint api for fetching days, hours, subjects, classes, types,etc

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -20,12 +20,52 @@ namespace timetable.Controllers
             this._mapper = mapper;
         }
 
+        [HttpGet("/api/ditet")]
+        public async Task<IEnumerable<DitetResource>> TerhiqDitet()
+        {
+            var ditet = await _context.Ditet.ToListAsync();
+
+            return _mapper.Map<List<Ditet>, List<DitetResource>>(ditet);
+        }
+
         [HttpGet("/api/lendet")]
         public async Task<IEnumerable<LendetResource>> TerhiqLendet()
         {
-            var features = await _context.Lendet.ToListAsync();
+            var lendet = await _context.Lendet.ToListAsync();
 
-            return _mapper.Map<List<Lendet>, List<LendetResource>>(features);
+            return _mapper.Map<List<Lendet>, List<LendetResource>>(lendet);
+        }
+
+        [HttpGet("/api/deget")]
+        public async Task<IEnumerable<DegetResource>> TerhiqDeget()
+        {
+            var deget = await _context.Deget.ToListAsync();
+
+            return _mapper.Map<List<Deget>, List<DegetResource>>(deget);
+        }
+
+        [HttpGet("/api/klasat")]
+        public async Task<IEnumerable<KlasatResource>> TerhiqKlasat()
+        {
+            var klasat = await _context.Klasat.ToListAsync();
+
+            return _mapper.Map<List<Klasat>, List<KlasatResource>>(klasat);
+        }
+
+        [HttpGet("/api/oret")]
+        public async Task<IEnumerable<OretResource>> TerhiqOret()
+        {
+            var oret = await _context.Oret.ToListAsync();
+
+            return _mapper.Map<List<Oret>, List<OretResource>>(oret);
+        }
+
+        [HttpGet("/api/tipet")]
+        public async Task<IEnumerable<TipetResource>> TerhiqTipet()
+        {
+            var tipet = await _context.Tipet.ToListAsync();
+
+            return _mapper.Map<List<Tipet>, List<TipetResource>>(tipet);
         }
     }
 }
